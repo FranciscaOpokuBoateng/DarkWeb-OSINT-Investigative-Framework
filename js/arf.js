0ae2d55185193c9e6049a8a80161e60fbe93ef6b
FranciscaOpokuBoateng <78004253+FranciscaOpokuBoateng@users.noreply.github.com>
2021-02-14 16:42:21 +0000

    update canvas width

--- js/arf.js
+++ js/arf.js
@@ -1,5 +1,5 @@
-var margin = [20, 120, 20, 140],
-    width = 1380 - margin[1] - margin[3],
+var margin = [20, 120, 20, 100],
+    width = 1500 - margin[1] - margin[3],
     height = 800 - margin[0] - margin[2],
     i = 0,
     duration = 1250,
